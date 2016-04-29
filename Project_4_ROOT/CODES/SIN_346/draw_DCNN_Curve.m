4ae18043c561423eb5986e4bd2f894826d6d840c
zhanghaoinf <zhanghaoinf@gmail.com>
2016-04-29 11:19:16 +0800

    Update draw_DCNN_Curve for DRN, Save settings of prototxt of Attempt_1

--- Project_4_ROOT/CODES/SIN_346/draw_DCNN_Curve.m
+++ Project_4_ROOT/CODES/SIN_346/draw_DCNN_Curve.m
@@ -1,54 +1,29 @@
 clear;
 close all;
 clc;
-fin = fopen('CAFFE_LOG.txt.test');
+testFile = './Attempt_1/CAFFE_LOG.txt.test';
 count = 0;
-while ~feof(fin)
-	tline = fgetl(fin);
-	if count > 0
-		S = textscan(tline,'%s %s %s %s');
-		iters(count, 1) = str2num(S{1,1}{1,1});
-		Acc(count, 1)   = str2num(S{1,3}{1,1});
-	end
-	count = count + 1;
-end
+[NumIters, Seconds, LearnRate, Acc, Loss] = textread(testFile,'%s%s%s%s%s','delimiter',',');
+iters = str2double(NumIters(2:length(NumIters)));
+tstAcc = str2double(Acc(2:length(Acc)));
+tstLoss = str2double(Loss(2:length(Loss)));
 subplot(2,2,1)
-plot(iters, Acc);
+plot(iters, tstAcc);
 xlabel('iter', 'Fontsize',20);
 ylabel('Accuracy','Fontsize',20);
 
-clear;
-fin = fopen('CAFFE_LOG.txt.test');
-count = 0;
-while ~feof(fin)
-	tline = fgetl(fin);
-	if count > 0
-		S = textscan(tline,'%s %s %s %s');
-		iters(count, 1) = str2num(S{1,1}{1,1});
-		Acc(count, 1)   = str2num(S{1,4}{1,1});
-	end
-	count = count + 1;
-end
 subplot(2,2,2)
-plot(iters, Acc);
+plot(iters, tstLoss);
 xlabel('iter', 'Fontsize',20);
 ylabel('Testing Loss','Fontsize',20);
 
-clear
-fin = fopen('CAFFE_LOG.txt.train');
-count = 0;
-while ~feof(fin)
-	tline = fgetl(fin);
-	if count > 0
-		S = textscan(tline,'%s %s %s %s');
-		iters(count, 1) = str2num(S{1,1}{1,1});
-		loss(count, 1)   = str2num(S{1,3}{1,1});
-	end
-	count = count + 1;
-end
+trnFile = './Attempt_1/CAFFE_LOG.txt.train';
+[NumIters, Seconds, LearnRate, Loss] = textread(trnFile,'%s%s%s%s','delimiter',',');
+iters = str2double(NumIters(2:length(NumIters)));
+trnLoss = str2double(Loss(2:length(Loss)));
 subplot(2,2,3)
-plot(iters(1:10:end), loss(1:10:end));
+plot(iters, trnLoss);
 
 xlabel('iter', 'Fontsize',20);
 ylabel('Training Loss','Fontsize',20);
-print('-depsc','loss_without_ignore.eps')
+print('-depsc','./Attempt_1/loss_without_ignore.eps')
