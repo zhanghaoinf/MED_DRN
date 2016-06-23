d24fa2dab46c56640bd9e81be84f2690a44720c2
zhanghaoinf@mac <zhanghaoinf@gmail.com>
2016-06-23 13:16:38 +0800

    update DRN50_L5B in Backup

---
+++ Project_2_ROOT/CODES/BACKUP/DRN50_L5B/st_0_spMax_BG.py
@@ -0,0 +1,72 @@
+#!/usr/bin/env python
+import scipy.io as sio
+import numpy as np
+import os
+import sys
+import string
+from subroutine import maxPool3DMat, createFolder
+
+dataRoot     = '/home/hzhang57/NEW_WORLD/Project_2_ROOT/DATA'
+trecvidFolder = '/home/hzhang57/Data/MED'
+###----------only modify this part-----------------------
+fmt = '.mat'
+tarFmt = '.7z'
+feature = 'DRN50_L5B'
+dataSet = 'Event_BG'
+###---------------------------------------------
+fileList   = os.path.join(trecvidFolder, 'STRUCTURE', dataSet+'.txt')
+featureFolder = os.path.join(trecvidFolder,'Features', dataSet, feature)
+createFolder(os.path.join(dataRoot, 'SPMAX'))
+createFolder(os.path.join(dataRoot, 'SPMAX', dataSet))
+outFolder     = os.path.join(dataRoot, 'SPMAX',dataSet, feature)
+createFolder(outFolder)
+
+ArgvLen = len(sys.argv)
+if ArgvLen!= 3:
+	print ("-" * 30)
+	print("Number of argv is not correct\n please specify start and end number")
+	print ("-" * 30)
+	exit(0)
+stLine = string.atoi(sys.argv[1])
+edLine = string.atoi(sys.argv[2])
+cntRange = range(stLine-1, edLine)
+fid = open(fileList)
+vidList = fid.readlines()
+vidNum  = len(vidList)
+Disp = '|' + str(vidNum) + '|' + str(edLine) + '----' + str(stLine -1) + ':'
+for cnt in cntRange:
+	print Disp + str(cnt)
+	vid = vidList[cnt].strip()
+	outFeatPath = os.path.join(outFolder, vid + fmt)
+	if os.path.exists(outFeatPath):
+		continue
+	else:
+		vidFeatFile = os.path.join(featureFolder, vid + tarFmt)
+		cmd1 = '7z x ' + vidFeatFile + ' -o./temp/'
+		os.system(cmd1)
+		vidTempFile =  os.path.join('./temp', vid + fmt)
+		vidMatTemp  = sio.loadmat(vidTempFile)
+		cmd2 = 'rm ./temp/' + vid + fmt
+		os.system(cmd2) 
+		######################################
+		vidMat      = vidMatTemp['F5B']
+		######################################
+		[Dim, H, nW] = vidMat.shape
+		n = nW / H
+		for sigKF in range(1, n+1):
+			id1 = sigKF * H
+			id2 = sigKF * H - H
+			sigFeat = vidMat[:,:,id2:id1]
+			sp1   = maxPool3DMat(sigFeat, (7,7),1)
+			sp2   = maxPool3DMat(sigFeat, (6,6),1)
+			sp3   = maxPool3DMat(sigFeat, (5,5),1)
+			sp4   = maxPool3DMat(sigFeat, (2,2),1)
+			spFeat = np.concatenate((sp1, sp2, sp3, sp4),axis=0)
+			if sigKF == 1:
+				vidFeat = np.array(spFeat)
+			else:
+				vidFeat = np.concatenate((vidFeat, spFeat), axis=0)
+		sio.savemat(outFeatPath, {'vidFeat':vidFeat})
+
+
+
---
+++ Project_2_ROOT/CODES/BACKUP/DRN50_L5B/st_4_collectBG.m
@@ -0,0 +1,37 @@
+function main()
+addpath('../../');
+paramInit
+% Feature Config
+Feature = 'DRN50_L5B';
+Dim     = 256*256;
+
+CEventID = ['Event_BG'];
+% Infolder
+InFolder  = [ dataRoot, Feature, '_VLAD_ENC/', CEventID, '/'];
+% Outfolder
+OutFolder = [ dataRoot, Feature, '_COLLECTION_ENC/'];
+dirmake(OutFolder);
+OutFile = [ OutFolder, CEventID, '_VLAD.mat' ]; 
+% Structure 
+VidListFile = [ tvMed, 'STRUCTURE/',CEventID,'.txt'];
+VidList     = textread(VidListFile, '%s','delimiter','\n');
+VidNum      = length(VidList);
+
+BG_MAT      = zeros(VidNum, Dim);
+BG_Cnt = 1;
+for VidCnt = 1: VidNum
+	fprintf('%d %d %d\n', VidNum, VidCnt, 1);
+	Vid = strtrim(char(VidList{VidCnt}));
+	VidFeatFile = [ InFolder, Vid, '.txt' ];
+	if ~exist(VidFeatFile)
+		continue;
+	end
+	VidFeat     = csvread(VidFeatFile);
+	BG_MAT(BG_Cnt,:) = VidFeat;
+	BG_Cnt = BG_Cnt + 1;
+end
+	if BG_Cnt < VidNum
+		BG_MAT(BG_Cnt:VidNum,:) = [];
+	end
+	save(OutFile, 'BG_MAT','-v7.3');
+end
